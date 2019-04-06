033c74d72625dcbf90c1e41d6a7a7569bb32c94f
Dileep Nandanam <dileep@Dileeps-MacBook-Air.local>
2019-04-05 20:33:11 -0700

    default order

--- app/models/catagory.rb
+++ app/models/catagory.rb
@@ -1,6 +1,7 @@
 class Catagory < ApplicationRecord
   has_many :plans
   has_many :photos
+  default_scope -> {order('id asc')}
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
